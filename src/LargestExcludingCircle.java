e67c02a9c6fa22c28f1213f0f9d857f3dd3cb893
sean <seanl.12090@gmail.com>
2019-03-07 21:58:39 +1300

    Moved all classes back to one file for easier readability. Tidied up code and comments

--- src/LargestExcludingCircle.java
+++ src/LargestExcludingCircle.java
@@ -1,34 +1,26 @@
 /*
- * Smallest enclosing circle - Library (Java)
+ * Sean Lau
+ * Zade Fairweather
+ * Oscar Allen
+ * Max Stewart
  *
- * Copyright (c) 2018 Project Nayuki
- * https://www.nayuki.io/page/smallest-enclosing-circle
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program (see COPYING.txt and COPYING.LESSER.txt).
- * If not, see <http://www.gnu.org/licenses/>.
+ * References: https://www.nayuki.io/res/smallest-enclosing-circle/SmallestEnclosingCircleTest.java
+ * https://en.wikipedia.org/wiki/Circumscribed_circle
  */
 
+
 import java.io.IOException;
+import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public final class LargestExcludingCircle {
-    private static List<String> inputs;
     private static List<Point> points;
+    private static String fileName = "coords.txt";
 
     public static void main(String[] args) {
         try {
@@ -38,29 +30,30 @@ public final class LargestExcludingCircle {
 
             //makes the smallest enclosing circle possible
             Circle smallestEnclosing = makeCircle(points);
-            System.out.println(smallestEnclosing);
 
             //find the largest excluding circle
             Circle largestExcluding = largest(smallestEnclosing, false, smallestEnclosing.shrink().grow());
             //print a summary
-            System.out.println(String.format("The largest circle that can drawn such that no more than eleven points are" +
+            System.out.println(String.format("The largest circle that can be drawn such that no more than eleven points are" +
                             " within its area is of radius %.2fm.\nThe ideal circle is centred on %.2fm north and %.2fm" +
-                            " east of the central point", largestExcluding.r, largestExcluding.c.x, largestExcluding.c.y));
+                            " east of the central point", largestExcluding.radius, largestExcluding.center.x, largestExcluding.center.y));
 
-            //lists the excluded points
+            /*
+            //finds the excluded points
             List<Point> excluded = new ArrayList<>();
             for(Point individual : points) {
                 if(!largestExcluding.contains(individual)) {
                     excluded.add(individual);
                 }
             }
-            //prints the excluded points
+            //prints the excluded points - testing purposes
             for (Point a : excluded){
                 System.out.println(a + " is not within this circle");
             }
+            */
 
         } catch (IOException e) {
-            System.out.println("File not found");
+            System.out.println(String.format("Error, %s not found", fileName));
         }
     }
 
@@ -72,22 +65,22 @@ public final class LargestExcludingCircle {
         else newCircle = old.shrink();
 
         //checks to see how many points are excluded
-        int j = 0;
+        int i = 0;
         for(Point individual : points) {
-            if(!newCircle.contains(individual)) {
-                j++;
+            if(newCircle.doesNotContain(individual)) {
+                i++;
             }
         }
         //if the radius is the original, return a circle of radius 0.01 smaller
-        if(newCircle.r == original.r) {
+        if(newCircle.radius == original.radius) {
             return newCircle.shrink();
         }
         //else if there are more than one points excluded, then see if you can enlarge the circle and still exclude at least one
-        else if(j > 1) {
+        else if(i > 1) {
             return largest(newCircle, true, original);
         }
         //if there are no points excluded, then shrink the circle
-        else if(j == 0) {
+        else if(i == 0) {
             return largest(newCircle, false, original);
         }
         //else the new circle is right
@@ -98,82 +91,90 @@ public final class LargestExcludingCircle {
      * Reads the file coords.txt
      */
     private static void readFile() throws IOException {
-        inputs = Files.readAllLines(Paths.get("coords.txt"));
+        List<String> inputs = Files.readAllLines(Paths.get(fileName));
+        List<Point> localPoints = new ArrayList<>();
         for (String line : inputs) {
             if (!line.equals("Telephone sites")) {
                 String[] s = line.split(" ");
-                points.add(new Point(Double.parseDouble(s[0]), Double.parseDouble(s[1])));
+                localPoints.add(new Point(Double.parseDouble(s[0]), Double.parseDouble(s[1])));
             }
         }
+        setPoints(localPoints);
     }
 
     /*
-     * Returns the smallest circle that encloses all the given points. Runs in expected O(n) time, randomized.
+     * Returns the smallest circle that encloses all the given points. Expected run time of O(n)
+     * Initially: No boundary points known
      */
-    // Initially: No boundary points known
+
     private static Circle makeCircle(List<Point> points) {
-        // Clone list to preserve the input data
+        // Copy list to preserve the data field
         List<Point> localPoints = new ArrayList<>(points);
 
-        // Progressively add points to circle or recompute circle
-        Circle c = null;
+        // Progressively check if points are within circle, recalculate circle if necessary
+        Circle localCircle = null;
         for (int i = 0; i < localPoints.size(); i++) {
             Point p = localPoints.get(i);
-            if (c == null || !c.contains(p))
-                c = makeCircleOnePoint(localPoints.subList(0, i + 1), p);
-        }
-        return c;
-    }
-
-
-    // One boundary point known
-    private static Circle makeCircleOnePoint(List<Point> points, Point p) {
-        Circle c = new Circle(p, 0);
-        for (int i = 0; i < points.size(); i++) {
-            Point q = points.get(i);
-            if (!c.contains(q)) {
-                if (c.r == 0)
-                    c = makeDiameter(p, q);
-                else
-                    c = makeCircleTwoPoints(points.subList(0, i + 1), p, q);
+            if (localCircle == null || localCircle.doesNotContain(p)) {
+                localCircle = makeCircle(localPoints.subList(0, i + 1), p);
             }
         }
-        return c;
+        return localCircle;
     }
 
 
-    // Two boundary points known
-    private static Circle makeCircleTwoPoints(List<Point> points, Point p, Point q) {
-        Circle circ = makeDiameter(p, q);
+    /*
+     * If one edge point is known, check to see if the circle contains the remaining points. If not, then resize the circle
+     */
+    private static Circle makeCircle(List<Point> points, Point p1) {
+        Circle localCircle = new Circle(p1, 0);
+        for (int i = 0; i < points.size(); i++) {
+            Point p2 = points.get(i);
+            if (localCircle.doesNotContain(p2)) {
+                if (localCircle.radius == 0)
+                    localCircle = makeDiameter(p1, p2);
+                else
+                    localCircle = makeCircle(points.subList(0, i + 1), p1, p2);
+            }
+        }
+        return localCircle;
+    }
+
+
+    /*
+    * If two edge points are known, check to see if the circle contains the remaining points. If not, then resize the circle
+    */
+    private static Circle makeCircle(List<Point> points, Point p1, Point p2) {
+        Circle localCircle = makeDiameter(p1, p2);
         Circle left  = null;
         Circle right = null;
 
-        // For each point not in the two-point circle
-        Point pq = q.subtract(p);
-        for (Point r : points) {
-            if (circ.contains(r))
-                continue;
 
-            // Form a circumcircle and classify it on left or right side
-            double cross = pq.cross(r.subtract(p));
-            Circle c = makeCircumcircle(p, q, r);
-            if (c != null) {
-                if (cross > 0 && (left == null || pq.cross(c.c.subtract(p)) > pq.cross(left.c.subtract(p))))
-                    left = c;
-                else if (cross < 0 && (right == null || pq.cross(c.c.subtract(p)) < pq.cross(right.c.subtract(p))))
-                    right = c;
+        Point p3 = p2.subtract(p1);
+        for (Point r : points) {
+            // For each point not in the circle
+            if (localCircle.doesNotContain(r)) {
+                // Form a circumcircle and classify it being on the left or right of the origin
+                double determinant = p3.cross(r.subtract(p1));
+                Circle c = makeCircumcircle(p1, p2, r);
+                if (c != null) {
+                    if (determinant > 0 && (left == null || p3.cross(c.center.subtract(p1)) > p3.cross(left.center.subtract(p1))))
+                        left = c;
+                    else if (determinant < 0 && (right == null || p3.cross(c.center.subtract(p1)) < p3.cross(right.center.subtract(p1))))
+                        right = c;
+                }
             }
         }
 
         // Select which circle to return
         if (left == null && right == null)
-            return circ;
+            return localCircle;
         else if (left == null)
             return right;
         else if (right == null)
             return left;
         else
-            return left.r <= right.r ? left : right;
+            return left.radius <= right.radius ? left : right;
     }
 
 
@@ -185,14 +186,15 @@ public final class LargestExcludingCircle {
 
     private static Circle makeCircumcircle(Point a, Point b, Point c) {
         // Mathematical algorithm from Wikipedia: Circumscribed circle
+
         //"recenter" the three points on the minimum x and y values
-        double originx = (Math.min(Math.min(a.x, b.x), c.x) + Math.max(Math.min(a.x, b.x), c.x)) / 2;
-        double originy = (Math.min(Math.min(a.y, b.y), c.y) + Math.max(Math.min(a.y, b.y), c.y)) / 2;
+        double originX = (Math.min(Math.min(a.x, b.x), c.x) + Math.max(Math.min(a.x, b.x), c.x)) / 2,
+                originY = (Math.min(Math.min(a.y, b.y), c.y) + Math.max(Math.min(a.y, b.y), c.y)) / 2;
 
         //get the new co-ordinates in relation to this new origin
-        double ax = a.x - originx,  ay = a.y - originy;
-        double bx = b.x - originx,  by = b.y - originy;
-        double cx = c.x - originx,  cy = c.y - originy;
+        double ax = a.x - originX,  ay = a.y - originY,
+                bx = b.x - originX,  by = b.y - originY,
+                cx = c.x - originX,  cy = c.y - originY;
 
         //find the scale of the triangle
         double d = (ax * (by - cy) + bx * (cy - ay) + cx * (ay - by)) * 2;
@@ -200,16 +202,92 @@ public final class LargestExcludingCircle {
         if (d == 0) return null;
 
         //finds the intersection of the bisectors of the three sides of the triangle - the center of a circumscribed circle
-        double x = ((ax*ax + ay*ay) * (by - cy) + (bx*bx + by*by) * (cy - ay) + (cx*cx + cy*cy) * (ay - by)) / d;
-        double y = ((ax*ax + ay*ay) * (cx - bx) + (bx*bx + by*by) * (ax - cx) + (cx*cx + cy*cy) * (bx - ax)) / d;
+        double x = ((ax*ax + ay*ay) * (by - cy) + (bx*bx + by*by) * (cy - ay) + (cx*cx + cy*cy) * (ay - by)) / d,
+                y = ((ax*ax + ay*ay) * (cx - bx) + (bx*bx + by*by) * (ax - cx) + (cx*cx + cy*cy) * (bx - ax)) / d;
 
-        //adds the origin points back on
-        Point p = new Point(originx + x, originy + y);
-        //makes the radius the distance from the center to the three points of the triangle
+        //makes a new point based on the calculated values and from the 'origin'
+        Point p = new Point(originX + x, originY + y);
+
+        //makes the radius the distance from the center to the furthest of the three points of the triangle
         double r = Math.max(Math.max(p.distance(a), p.distance(b)), p.distance(c));
         return new Circle(p, r);
     }
 
+    //setter to satisfy IntelliJ
+    private static void setPoints(List<Point> points) {
+        LargestExcludingCircle.points = points;
+    }
+
+    static final class Circle {
+        private DecimalFormat df = new DecimalFormat("#.##");
+
+        final Point center;
+        final double radius;
+
+        //setter
+        Circle(Point center, double radius) {
+            this.center = center;
+            this.radius = radius;
+        }
+
+        //checks if the point is within the circle
+        boolean doesNotContain(Point p) {
+            return !(center.distance(p) <= radius * (1 + 1e-14));
+        }
+
+        @Override
+        public String toString() {
+            return String.format("LargestExcludingCircle.Circle(x=%g, y=%g, radius=%g)", center.x, center.y, radius);
+        }
+
+        //shrink and grow are used to change the size of the smallest enclosing circle to find the largest excluding circle
+        Circle shrink() {
+            df.setRoundingMode(RoundingMode.CEILING);
+            return new Circle(center.round(), Double.parseDouble(df.format(radius)) - 0.01);
+        }
+
+        Circle grow() {
+            return new Circle(center, radius + 0.01);
+        }
+    }
+}
+
+final class Point {
+    private static DecimalFormat df = new DecimalFormat("#.##");
+
+    final double x;
+    final double y;
+
+    //setter
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //subtract a point from another
+    Point subtract(Point p) {
+        return new Point(x - p.x, y - p.y);
+    }
+
+    //find the distance between two points
+    double distance(Point p) {
+        return Math.hypot(x - p.x, y - p.y);
+    }
+
+    //determinant
+    double cross(Point p) {
+        return x * p.y - y * p.x;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Point(%g, %g)", x, y);
+    }
+
+    //round returns a point in 2 dp as the input file is in 2 dp
+    Point round() {
+        return new Point(Double.parseDouble(df.format(x)), Double.parseDouble(df.format(y)));
+    }
 }
 
 
